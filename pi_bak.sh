00f2c9f0fd7f4c5d1168c224106993ff3968cec1
Travis Austin <travis.austin@travisofaustin.com>
2017-09-11 13:21:35 -0500

    updated backup method

--- pi_bak.sh
+++ pi_bak.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # *****
 # change directory to rsync folder (taken from stackoverflow - verify if this step is necessary
-cd /home/rsync_pi
+cd /rsync_pi
 
-# backup home as a .tar.gz file, and put it in the rsync folder
-sudo tar czf pi_bak.tar.gz /home/rsync_pi
+##### FILESYSTEM BACKUP #####
+# OLD METHOD - backup home as a .tar.gz file, and put it in the rsync folder
+#sudo tar czf pi_bak.tar.gz /home/rsync_pi
 
-# backup everything, excluding folders in exclude file
-# initial run will take a long time; subsequent runs will utilize excludes
-# !BACKUP EVERYTHING MANUALLY BEFORE LETTING IT CONTINUE INAUGURAL RUN
-# sudo rsync -aHv --delete-during --exclude-during --exclude-from=/home/,dotlifes/rsync-exclude.txt / /home/rsync_pi/
-
+# CURRENT - utilize rsync and a text file with exclusions to back up everything
+# directly to a mounted USB drive in /mnt/usbdrive
+rsync -aHv --delete-during --exclude-from=/rsync-exclude.txt / /mnt/usbdrive/
+# note that "--delete-during" is meaningless the 1st time, but is used in
+# subsequent backups
+# TO RESTORE:
+#rsync -av --delete-during /mnt/usbdrive/ /mnt/sdcard_partition2
