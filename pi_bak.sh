179ab781dd030486c913c32d472700dc9f55818e
Travis Austin <travis.austin@travisofaustin.com>
2017-09-11 15:47:15 -0500

    update all

--- pi_bak.sh
+++ pi_bak.sh
@@ -3,14 +3,20 @@
 # change directory to rsync folder (taken from stackoverflow - verify if this step is necessary
 cd /rsync_pi
 
-##### FILESYSTEM BACKUP #####
-# OLD METHOD - backup home as a .tar.gz file, and put it in the rsync folder
+###### FILESYSTEM BACKUP ######
+### Depreciated ### 
+# backup home as a .tar.gz file, and put it in the rsync folder
 #sudo tar czf pi_bak.tar.gz /home/rsync_pi
 
-# CURRENT - utilize rsync and a text file with exclusions to back up everything
-# directly to a mounted USB drive in /mnt/usbdrive
-rsync -aHv --delete-during --exclude-from=/rsync-exclude.txt / /mnt/usbdrive/
-# note that "--delete-during" is meaningless the 1st time, but is used in
-# subsequent backups
-# TO RESTORE:
+### CURRENT ###
+# Utilize rsync and a text file with exclusions to back up everything directly to a mounted USB drive in /mnt/usbdrive
+sudo rsync -aHv --delete-during --exclude-from=/rsync-exclude.txt / /mnt/usbdrive/
+# note that "--delete-during" is meaningless the 1st time, but is used in subsequent backups
+# TO RESTORE, use following at cmd prompt (not in this script):
 #rsync -av --delete-during /mnt/usbdrive/ /mnt/sdcard_partition2
+
+# Compress output of rsync above to .tar file - CONFIRMED: WORKS
+sudo /home/.dotfiles/pi_bak.sh > /rsync_pi/pi_bak-`date +\%Y\%m\%d\%H\%M`.tar.gz
+
+# Copy 'rsync_pi/" over to usb "rsync_pi_usb/"
+sudo cp -r /rsync_pi /mnt/usbdrive/rsync_pi_usb/
---
+++ pi_bak_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# *****
+# change directory to rsync folder (taken from stackoverflow - verify if this step is necessary
+cd /rsync_pi
+
+##### FILESYSTEM BACKUP #####
+# OLD METHOD - backup home as a .tar.gz file, and put it in the rsync folder
+#sudo tar czf pi_bak.tar.gz /home/rsync_pi
+
+# CURRENT - utilize rsync and a text file with exclusions to back up everything
+# directly to a mounted USB drive in /mnt/usbdrive
+rsync -aHv --delete-during --exclude-from=/rsync-exclude.txt / /mnt/usbdrive/
+# note that "--delete-during" is meaningless the 1st time, but is used in
+# subsequent backups
+# TO RESTORE:
+#rsync -av --delete-during /mnt/usbdrive/ /mnt/sdcard_partition2
+
+# TEST: output above in compressed file
+/home/.dotfiles/pi_bak.sh > /rsync_pi/`date +\%Y\%m\%d\%H\%M\%S`-pi_bak.tar.gz
